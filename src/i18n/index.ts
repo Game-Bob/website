5ac0fb78c91b74ca9f44289c6ae7c7ad5bd4d49c
jjlmoya <juanjo1989@gmail.com>
2026-04-03 14:16:30 +0200

    feat: add banner apps

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,20 +1,24 @@
 import enHome from "./locales/en/home";
 import enMenu from "./locales/en/menu";
 import enUI from "./locales/en/ui";
+import enStore from "./locales/en/store";
 import frHome from "./locales/fr/home";
 import frMenu from "./locales/fr/menu";
 import frUI from "./locales/fr/ui";
+import frStore from "./locales/fr/store";
 
 export const translations = {
     en: {
         home: enHome,
         menu: enMenu,
         ui: enUI,
+        store: enStore,
     },
     fr: {
         home: frHome,
         menu: frMenu,
         ui: frUI,
+        store: frStore,
     },
 } as const;
 
