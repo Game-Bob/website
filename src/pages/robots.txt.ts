6e2fae78feb70738e0216e4441dd1ed5d8476c7a
jjlmoya <juanjo1989@gmail.com>
2026-04-21 08:59:54 +0200

    chore: add verification webmaster tool

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,29 +1,56 @@
 import type { APIRoute } from 'astro';
 
-const robots = `User-agent: Baiduspider
-Allow: /
-Crawl-delay: 1
-Disallow: /*/widgets?*
-
-User-agent: Googlebot
+const robots = `User-agent: Googlebot
 Allow: /
 Disallow: /*/widgets?*
 
 User-agent: Bingbot
+Allow: /
+Disallow: /*/widgets?*
+
+User-agent: Baiduspider
+Allow: /
 Crawl-delay: 1
+Disallow: /*/widgets?*
+
+User-agent: PetalBot
 Allow: /
 Disallow: /*/widgets?*
 
 User-agent: Applebot
 User-agent: DuckDuckBot
-User-agent: Yandexbot
 Allow: /
 
+User-agent: Yandexbot
+Allow: /
+Clean-param: utm_source&utm_medium&utm_campaign&utm_content&utm_term&sort&filter /
+
+User-agent: SeznamBot
+Allow: /
+Disallow: /*/widgets?*
+
 User-agent: MJ12bot
+Disallow: /
+
 User-agent: AhrefsBot
+Disallow: /
+
 User-agent: SemrushBot
+Disallow: /
+
 User-agent: DotBot
+Disallow: /
+
 User-agent: MojeekBot
+Disallow: /
+
+User-agent: DataForSeoBot
+Disallow: /
+
+User-agent: BLEXBot
+Disallow: /
+
+User-agent: MegaIndex
 Disallow: /
 
 User-agent: *
@@ -32,7 +59,6 @@ Disallow: /*?*sort=
 Disallow: /*?*filter=
 Disallow: /*?*utm_
 Disallow: /*/widgets?*
-Crawl-delay: 1
 
 Sitemap: https://www.gamebob.dev/sitemap-index.xml
 `;
