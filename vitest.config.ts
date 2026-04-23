13842ffffd512b20f6cf7eae220cacafebe521eb
jjlmoya <juanjo1989@gmail.com>
2026-04-23 23:15:50 +0200

    feat: add new tools

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         environment: 'node',
-        testTimeout: 120000,
+        testTimeout: 300000,
     },
     plugins: [
         {
